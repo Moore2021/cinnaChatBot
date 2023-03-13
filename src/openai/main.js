3a4a4d249acb6043a438929e8d870d9ab7e39d75
Andrew <thefryingpan2021@gmail.com>
2023-03-12 20:17:34 -0400

    cleaning

--- src/openai/main.js
+++ src/openai/main.js
@@ -1,11 +1,12 @@
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
 	apiKey: process.env.OPENAI_API_KEY,
+	organization: `org-qBMkn85qHyMzyS5dCvJLnvWl`,
 });
 const openai = new OpenAIApi(configuration);
-const fs = require(`fs`)
+const { replaceRepeats, formatDate, deleteHistory, recentChatters, peopleInVC, writeToConvoLog, prepareUserMsg } = require(`../utils/openai.js`);
 /**
- * Output bot's latency
+ * Return a response from the openAi Lib
  * @author Pan
  */
 module.exports = {
@@ -14,152 +15,83 @@ module.exports = {
 	description: `Get a response from OpenAi`,
 	permissionLevel: 0,
 	async execute(message, client) {
-		const PreviousOpenAiModel = client.pastConvo
+		var PreviousOpenAiModelMsgs = client.pastConvo.messages
 		const Nicknames = client.nicknames
 		const startOpenAiModel = {
 			"messages":
 				[
 					{
-						"role":"system",
-						"content":`You're on a roleplay, Your name is Pochita, you act like a talking dog and bark Wauf! your owner, master, and favourite is Cinna or cinna.`
+						"role": "system",
+						"content": `You're on a roleplay, Your name is Pochita, you act like a talking dog and bark Wauf! your owner, master, and favourite is Cinna or cinna.`
 					}
 				]
-			};
-
-		// Functions to use in file
-		const addSysContext = (str) =>{
-			return startOpenAiModel.messages[0].content+=`\n, ${str}`
+		};
+		const arrayMsg = startOpenAiModel.messages
+		/**
+		 * edit an object's inner property
+		 * @param {String} str
+		 * @returns {void}
+		 */
+		function addSysContext(str) {
+			return arrayMsg[0].content += `\n ${str}`.trim()
 		}
 
-		const DELETE_HISTORY = () =>{
-			const J = `{"messages":[]}`
-			client.pastConvo.messages = []
-			fs.writeFile('.data/pochitaConvo.json', J, 'utf8', (err) => {
-				if (err) return console.log(err);
-			});
-			message.channel.send(`I'm sorry but i think i was bonked by something and i forgot what we were saying`)
-		}
-		
-		const formatDate = (date) => {
-			function getTimezoneName() {
-				const today = new Date();
-				const short = today.toLocaleDateString(undefined);
-				const full = today.toLocaleDateString(undefined, { timeZoneName: 'long' });
-			  
-				// Trying to remove date from the string in a locale-agnostic way
-				const shortIndex = full.indexOf(short);
-				if (shortIndex >= 0) {
-				  const trimmed = full.substring(0, shortIndex) + full.substring(shortIndex + short.length);
-				  
-				  // by this time `trimmed` should be the timezone's name with some punctuation -
-				  // trim it from both sides
-				  return trimmed.replace(/^[\s,.\-:;]+|[\s,.\-:;]+$/g, '');
-			  
-				} else {
-				  // in some magic case when short representation of date is not present in the long one, just return the long one as a fallback, since it should contain the timezone's name
-				  return full;
-				}
-			  }
-			var hours = date.getHours();
-			var minutes = date.getMinutes();
-			var ampm = hours >= 12 ? 'pm' : 'am';
-			hours = hours % 12;
-			hours = hours ? hours : 12; // the hour '0' should be '12'
-			minutes = minutes < 10 ? '0'+minutes : minutes;
-			var strTime = hours + ':' + minutes + ' ' + ampm;
-			var timeZone = getTimezoneName()
-			return (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime + " " + timeZone;
-		  }
-
-		const whatNicknameToUse = async (user_id) => {
-			if (Nicknames[user_id]) return Nicknames[user_id]
-			const guildMember = await message.guild.members.fetch(user_id)
-			if (guildMember.nickname) return guildMember.nickname
-			return guildMember.displayName
-		}
-
-		// who was recently in chat
-		const recentMsg = await message.channel.messages.fetch({limit:10,cache:true})
-		var people = new Set(recentMsg.map(m=>{if (!m.author.bot) {return `${m.author.id}`}else{return}} ))
-		people = [...people].filter(Boolean)
-		const userNames_chat = []
-		for (let i = 0; i < people.length; i++) {
-			const nickToUse = await whatNicknameToUse(people[i])
-			userNames_chat.push(nickToUse)		
-		}
-		const peopleToString = userNames_chat.length >= 3 ? `${userNames_chat.slice(0,-1).join(`,`)} and ${userNames_chat.slice(-1)}` : userNames_chat.length >= 2 ? userNames_chat.join(` and `) : userNames_chat.toString()
-		
 		// Add system context
-		addSysContext(`Current date is: ${formatDate(new Date())}`)
-		addSysContext(`people in chat: ${peopleToString}`)
+		addSysContext(`Current date is: ${formatDate(new Date())}.`);
+
+		// Check who recent chatters in text chat are
+		const peopleToString = await recentChatters(message, Nicknames);
+		addSysContext(`people in text chat: ${peopleToString}.`);
 
 		// Fetch who's in VC
 		// If people are in the same VC
-		if (message.member.voice.channel){
-			const NicknameToUse = await whatNicknameToUse(message.author.id)
-			addSysContext(`${NicknameToUse} is connected to the vc ${message.member.voice.channel.name}`)
-			const membersInVC = message.member.voice.channel.members
-			var people_VC = new Set(membersInVC.map(m=>{if (!m.user.bot) {return `${m.user.id}`}else{return}} ))
-			people_VC = [...people_VC].filter(Boolean)
-			const userNames_VC = []
-			for (let i = 0; i < people_VC.length; i++) {
-				const nickToUse = await whatNicknameToUse(people_VC[i])
-				userNames_VC.push(nickToUse)		
-			}
-			const otherPeopleToString = userNames_VC.length >= 3 ? `${userNames_VC.slice(0,-1).join(`, `)} and ${userNames_VC.slice(-1)}` : userNames_VC.length >= 2 ? userNames_VC.join(` and `) : userNames_VC.toString()
-			addSysContext(`people in vc or voice chat: ${otherPeopleToString}`)
+		if (message.member.voice.channel) {
+			const otherPeopleToString = await peopleInVC(message, Nicknames);
+			addSysContext(`people in vc or voice chat: ${otherPeopleToString}.`);
 		}
 
-		const pastConvo = client.pastConvo.messages.length>1 ? client.pastConvo.messages : PreviousOpenAiModel.messages
-		const MAX_CONVOS = 6
-		
-		// Now add the user input
-		const enddingInstructions = ` - dont talk in quotes and change your pattern of talking`
+		const MAX_CONVOS = 6;
 
 		// Prepare the payload for api
-		const NicknameToUse = await whatNicknameToUse(message.author.id)
-		const msgFromUser = { "role": "user", "content": `${NicknameToUse} said: "${message.content.trim()}"${enddingInstructions}` }
-		var whatToSend = pastConvo.concat([msgFromUser])
-		if (whatToSend.length > MAX_CONVOS) whatToSend = whatToSend.slice(whatToSend.length - (MAX_CONVOS/2))
-		
-		if (client.pastConvo.messages.length<1){
-			client.pastConvo.messages = whatToSend
-		}else {
-			client.pastConvo.messages.push(msgFromUser)
+		const msgFromUser = await prepareUserMsg(message, Nicknames);
+		arrayMsg.push(msgFromUser)
+
+		const systemMsg = arrayMsg.length > MAX_CONVOS ? arrayMsg.slice(1, arrayMsg.length - (MAX_CONVOS / 2)) : arrayMsg.shift()
+
+		if (PreviousOpenAiModelMsgs.length < 1) {
+			PreviousOpenAiModelMsgs = arrayMsg;
+		} else {
+			PreviousOpenAiModelMsgs.push(msgFromUser);
 		}
-		
+
 		// If there are too many conversations cut them in half
-		message.channel.sendTyping() // To show bot is typing/thinking
-		
+		message.channel.sendTyping(); // To show bot is typing/thinking
+
 		try {
-			const finalJoin = startOpenAiModel.messages.concat(whatToSend)
-			console.log(whatToSend)
+			const finalJoin = [systemMsg].concat(PreviousOpenAiModelMsgs)
 			const completion = await openai.createChatCompletion({
 				model: "gpt-3.5-turbo",
 				max_tokens: 2000,
 				frequency_penalty: -1.6,
 				messages: finalJoin
-			},{timeout:50000});
+			}, { timeout: 1200000 });
 
 			// What the api returned
-			var response = completion.data.choices[0].message.content.trim()
-			if (response.length > 1500) response = response.slice(0, 1500) // If the response exceeds Discord's text limit
-			
+			var response = completion.data.choices[0].message.content.trim();
+			if (response.length > 1500) response = response.slice(0, 1500); // If the response exceeds Discord's text limit
+
 			// prepare bot's response
-			const msgFromBot = { "role": "assistant", "content": `${response}` }
+			const msgFromBot = { "role": "assistant", "content": `${replaceRepeats(response)}` }
 
 			// Prepare to save to conversation file
-			PreviousOpenAiModel.messages.push(msgFromBot)
-			whatToSend.push(msgFromBot)
+			PreviousOpenAiModelMsgs.push(msgFromBot);
 			// If there are too many conversations cut them in half
-			if (whatToSend.length > MAX_CONVOS) whatToSend = whatToSend.slice(whatToSend.length - (MAX_CONVOS/2))
+			PreviousOpenAiModelMsgs.length > MAX_CONVOS ? PreviousOpenAiModelMsgs.slice(PreviousOpenAiModelMsgs.length - (MAX_CONVOS / 2)) : PreviousOpenAiModelMsgs
 
 			// Write to conversation file
-			const J = JSON.stringify({messages: whatToSend})
-			fs.writeFile('.data/pochitaConvo.json', J, 'utf8', (err) => {
-				if (err) return console.log(err);
-			});
-			return message.channel.send(response)
+			writeToConvoLog('.data/pochitaConvo.json', PreviousOpenAiModelMsgs);
+
+			return message.channel.send(response);
 		} catch (error) {
 			if (error.response) {
 				console.log(error.response.status);
@@ -167,7 +99,9 @@ module.exports = {
 			} else {
 				console.log(error.message);
 			}
-			return DELETE_HISTORY();
+			return deleteHistory(client, message);
 		}
 	}
 }
+
+
---
+++ src/utils/openai.js
@@ -0,0 +1,172 @@
+const fs = require(`fs`)
+
+/**
+ * Continue an existing string
+ * @param {Object} ObjToAddTo 
+ * @param {String} str 
+ * @returns 
+ */
+function addSysContext(ObjToAddTo, str) {
+    return ObjToAddTo += `\n, ${str}`.trim()
+}
+
+/**
+ * Delete the chat history and send a message to let the end user know
+ * @param {Object} client 
+ * @param {Object} message 
+ * @returns {void}
+ */
+function deleteHistory(client, message) {
+    const J = `{"messages":[]}`;
+    client.pastConvo.messages = [];
+    fs.writeFile('.data/pochitaConvo.json', J, 'utf8', (err) => {
+        if (err)
+            return console.log(err);
+    });
+    message.channel.send(`I'm sorry but i think i was bonked by something and i forgot what we were saying`);
+    return
+}
+
+/**
+ * Return the current date and time in a readable format
+ * @param {Date} date 
+ * @returns {String}
+ */
+function formatDate(date) {
+    function getTimezoneName() {
+        const today = new Date()
+        const short = today.toLocaleDateString(undefined)
+        const full = today.toLocaleDateString(undefined, { timeZoneName: 'long' })
+
+        // Trying to remove date from the string in a locale-agnostic way
+        const shortIndex = full.indexOf(short)
+        if (shortIndex >= 0) {
+            const trimmed = full.substring(0, shortIndex) + full.substring(shortIndex + short.length)
+
+            // by this time `trimmed` should be the timezone's name with some punctuation -
+            // trim it from both sides
+            return trimmed.replace(/^[\s,.\-:;]+|[\s,.\-:;]+$/g, '')
+
+        } else {
+            // in some magic case when short representation of date is not present in the long one, just return the long one as a fallback, since it should contain the timezone's name
+            return full
+        }
+    }
+    if (!date) { 
+        date = new Date() 
+    }
+    var hours = date.getHours()
+    var minutes = date.getMinutes()
+    var ampm = hours >= 12 ? 'pm' : 'am'
+    hours = hours % 12
+    hours = hours ? hours : 12 // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes
+    var strTime = hours + ':' + minutes + ' ' + ampm
+    var timeZone = getTimezoneName()
+    return ((date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime + " " + timeZone).trim()
+}
+
+/**
+ * Return what nickname to use based on a predefined list
+ * @param {String} user_id 
+ * @param {Object} NICKNAMES 
+ * @param {Object} message
+ * @returns 
+ */
+async function whatNicknameToUse(user_id, NICKNAMES, message) {
+    if (NICKNAMES[user_id])
+        return NICKNAMES[user_id]
+    const guildMember = await message.guild.members.fetch(user_id)
+    if (guildMember.nickname)
+        return guildMember.nickname
+    return guildMember.displayName
+}
+
+/**
+ * Return a String based on if there are any people in chat
+ * @param {Object} message 
+ * @param {Object} Nicknames 
+ * @returns {String | null}
+ */
+async function recentChatters(message, Nicknames) {
+	const recentMsg = await message.channel.messages.fetch({ limit: 10, cache: true });
+	var people = new Set(recentMsg.map(m => { if (!m.author.bot) { return `${m.author.id}`; } else { return; } }));
+	people = [...people].filter(Boolean);
+	const userNames_chat = [];
+	for (let i = 0; i < people.length; i++) {
+		const nickToUse = await whatNicknameToUse(people[i], Nicknames, message);
+		userNames_chat.push(nickToUse);
+	}
+	const peopleToString = userNames_chat.length >= 3 ? `${userNames_chat.slice(0, -1).join(`,`)} and ${userNames_chat.slice(-1)}` : userNames_chat.length >= 2 ? userNames_chat.join(` and `) : userNames_chat.toString();
+	return peopleToString.trim();
+}
+
+/**
+ * Fetch who is in VC and return String
+ * @param {Object} message 
+ * @param {Object} Nicknames
+ */
+async function peopleInVC(message, Nicknames) {
+	const NicknameToUse = await whatNicknameToUse(message.author.id, Nicknames, message);
+	addSysContext(`${NicknameToUse} is connected to the vc ${message.member.voice.channel.name}`.trim());
+	const membersInVC = message.member.voice.channel.members;
+	var people_VC = new Set(membersInVC.map(m => { if (!m.user.bot) { return `${m.user.id}`; } else { return; } }));
+	people_VC = [...people_VC].filter(Boolean);
+	const userNames_VC = [];
+	for (let i = 0; i < people_VC.length; i++) {
+		const nickToUse = await whatNicknameToUse(people_VC[i], Nicknames, message);
+		userNames_VC.push(nickToUse);
+	}
+	const otherPeopleToString = userNames_VC.length >= 3 ? `${userNames_VC.slice(0, -1).join(`, `)} and ${userNames_VC.slice(-1)}` : userNames_VC.length >= 2 ? userNames_VC.join(` and `) : userNames_VC.toString();
+	return otherPeopleToString.trim();
+}
+
+/**
+ * Take an object and write to file
+ * @param {Object} filePath
+ * @param {Object} whatToSend 
+ * @returns 
+ */
+function writeToConvoLog(filePath,whatToSend) {
+	const J = JSON.stringify({ messages: whatToSend });
+	return fs.writeFile(filePath, replaceRepeats(J), 'utf8', (err) => {
+		if (err)
+			return console.log(err);
+	});
+}
+
+/**
+ * Return what the user prompt is in format readdy for api
+ * @param {Object} message 
+ * @param {Object} Nicknames 
+ * @returns {Object}
+ */
+async function prepareUserMsg(message, Nicknames) {
+	const enddingInstructions = ` - dont talk in quotes and change your pattern of talking`
+	const NicknameToUse = await whatNicknameToUse(message.author.id, Nicknames, message);
+    message.content = replaceRepeats(message.content)
+	const msgFromUser = { "role": "user", "content": `${NicknameToUse} said: "${message.content.trim()}"${enddingInstructions}`.trim() };
+	return msgFromUser;
+}
+
+/**
+ * Return a String without repeating characters
+ * @param {String} response 
+ * @returns {String}
+ */
+function replaceRepeats(response) {
+	const regexForNonWordsConsective=/\B(\W+)(?:\W+\1\B)+/
+	return response.replace(regexForNonWordsConsective, ``);
+}
+
+module.exports = {
+    addSysContext,
+    formatDate,
+    whatNicknameToUse,
+    deleteHistory,
+    recentChatters,
+    peopleInVC,
+    writeToConvoLog,
+    prepareUserMsg,
+    replaceRepeats
+}
